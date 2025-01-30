5060ec7ff0756004e98df62412a888f31d66d0e7
debryu <nicola.debole@gmail.com>
2025-01-30 15:32:19 +0100

    celeba_mini custom size for each split

--- datasets/dataset_classes.py
+++ datasets/dataset_classes.py
@@ -108,7 +108,9 @@ class CelebAMini(Subset):
         download: bool = False,
         concepts: list = None,
         label:int = 20,
-        subset_indices = [0,40000]
+        train_subset_indices = [0,10000],
+        val_subset_indices = [0,1000],
+        test_subset_indices = [0,10000],
     ) -> None:
         '''
         concepts: list of concepts to use, by choosing the indexes of the celeba attributes
@@ -121,13 +123,19 @@ class CelebAMini(Subset):
         if split == 'train':
             self.data = CelebACustom(root=root,split=split,target_type=target_type,transform=transform,target_transform=target_transform,download=download,concepts=concepts,label=label)
             self.classes = self.data.classes
-            self.subset_indices = range(subset_indices[0],subset_indices[1])
+            self.subset_indices = range(train_subset_indices[0],train_subset_indices[1])
             super().__init__(self.data,self.subset_indices)
-        else:
+        elif split == 'val' or split == 'valid':
             self.data = CelebACustom(root=root,split=split,target_type=target_type,transform=transform,target_transform=target_transform,download=download,concepts=concepts,label=label)
             self.classes = self.data.classes
-            #self.subset_indices = range(subset_indices[0],subset_indices[1])
-            self.subset_indices = range(0,len(self.data))
+            self.subset_indices = range(val_subset_indices[0],val_subset_indices[1])
+            #self.subset_indices = range(0,len(self.data))
+            super().__init__(self.data,self.subset_indices)
+        elif split == 'test':
+            self.data = CelebACustom(root=root,split=split,target_type=target_type,transform=transform,target_transform=target_transform,download=download,concepts=concepts,label=label)
+            self.classes = self.data.classes
+            self.subset_indices = range(test_subset_indices[0],test_subset_indices[1])
+            #self.subset_indices = range(0,len(self.data))
             super().__init__(self.data,self.subset_indices)
         
       
