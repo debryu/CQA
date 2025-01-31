0cf8ef1a268e21257faf72490fafa67c4a6641d3
Debole Nicola (ACC0104657) <nicola.debole@unitn.it@clicgpu2.cimec.unitn.it>
2025-01-31 16:09:04 +0100

    Adding seed for reproducibilty

--- core/train_models.py
+++ core/train_models.py
@@ -3,6 +3,7 @@ import os
 from models.training import get_trainer
 from config import SAVED_MODELS_FOLDER, folder_naming_convention
 from utils.args_utils import save_args
+from utils.utils import set_seed
 import wandb
 
 def run(args):
@@ -13,6 +14,9 @@ def run(args):
     logger.debug(f"Created folder: {args.save_dir}")
     os.makedirs(args.save_dir, exist_ok=True)
     logger.info(f"Starting training model {args.model}")
+    if args.seed:
+        logger.info(f"#- Run {args.seed} -#")
+        set_seed(args.seed)
     if args.wandb:
         wand_run = wandb.init(
             # Set the project where this run will be logged
@@ -26,7 +30,7 @@ def run(args):
     train_fn = get_trainer(args)
     final_args = train_fn(args)
     if args.wandb:
-        wandb.config.update(final_args)
+        wandb.config.update(final_args, allow_val_change=True)
         wandb.finish()
     save_args(final_args)
     return
--- deleteme.py
+++ deleteme.py
@@ -1,6 +1,24 @@
 from datasets import get_dataset
+from torch.utils.data import Subset
+import torch
 
-cub = get_dataset('cub')
-cel = get_dataset('celeba')
-print(cub[0])
-print(cel[0])
+cel = get_dataset('celeba', split = 'train')
+id2 = range(25000,len(cel))
+id1 = range(25000)
+
+sample1 = Subset(cel, id1)
+sample2 = Subset(cel, id2)
+
+counts1 = torch.zeros(sample1[0][1].shape[0])
+counts2 = torch.zeros(sample1[0][1].shape[0])
+for img,c,l in sample1:
+    counts1 += c
+
+for img,c,l in sample2:
+    counts2 += c
+
+counts1 /= len(sample1)
+counts2 /= len(sample2)
+
+print(counts1)
+print(counts2)
